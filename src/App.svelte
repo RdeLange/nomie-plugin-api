780416062586c3357b1249801482ae3324bc16b4
RdeLange <ronald_de_lange@hotmail.com>
2023-01-08 22:29:22 +0100

    startup fix4

--- src/App.svelte
+++ src/App.svelte
@@ -71,7 +71,7 @@
   function clientRunning(){
     localStorage.setItem(CLIENT_RUNNING,new Date());
     setTimeout(()=>{
-      //ApiStore.init();
+      ApiStore.init();
     },5000);
     setInterval(()=>{
       localStorage.setItem(CLIENT_RUNNING,new Date());
